68c7d7de7f173450d33d56af5748c62b92c69aa3
Nilo Schwencke <ilonemail2@protonmail.com>
2020-02-01 11:45:49 +0100

    Moving options as dedicated variable

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+options="-V geometry:margin=1in -o --latex-engine=xelatex"
 if [[ $# -gt 1 ]]; then
-		pandoc "$1" -o --latex-engine=xelatex -o "$2"
+		$(pandoc "$1" $options -o "$2")
 elif [[ $# -gt 0 ]]; then
 		ext1=$(echo "$1" | grep -oP "[^\.]+$")
-		pandoc "$1" -o --latex-engine=xelatex -o "${1/%$ext1/pdf}"
+		$(pandoc "$1" $options -o "${1/%$ext1/pdf}")
 else
 		(>&2 echo "You did not provide any parameter")
 fi
